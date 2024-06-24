9247c8ecaef78515a41237fdec5f620c03d8f71d
tamayo2 <juan.tamayo3@utp.edu.co>
2024-06-24 00:05:40 -0500

    V1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
+import Search from "./Components/Search/Search";
+import Support from "./Components/Support/Support";
+import Info from "./Components/Info/Info";
+import Lounge from "./Components/Lounge/Lounge";
+import Travelers from "./Components/Travelers/Travelers";
+import Subscribe from "./Components/Subscribers/Subscribe";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Home />
+      <Search />
+      <Support />
+      <Info />
+      <Lounge />
+      <Travelers />
+      <Subscribe />
+      <Footer />
     </div>
   )
 }
