4b01cc1bf2b61b8c126f382f7cae4eb5df7099fb
tamayo2 <juan.tamayo3@utp.edu.co>
2024-06-24 00:40:21 -0500

    ajustes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { SiConsul } from "react-icons/si";
+import { BsPhoneVibrateFill } from "react-icons/bs";
+import { AiOutlineGlobal } from "react-icons/ai";
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='navBar flex'>
+            <div className="navBarOne flex">
+                <div>
+                <SiConsul />
+                </div>
+
+                <div className="none flex">
+                    <li className="flex"> <BsPhoneVibrateFill />Support</li>
+                    <li className="flex"><AiOutlineGlobal />Languages</li>
+                </div>
+
+                <div className="atb flex">
+                    <span>Sign in</span>
+                    <span>Sign Out</span>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
