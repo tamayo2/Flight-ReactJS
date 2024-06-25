00f0591b50bbcffea5bd4b2b2601a6e59c4b3472
tamayo2 <juan.tamayo3@utp.edu.co>
2024-06-24 21:13:05 -0500

    changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { SiConsul } from "react-icons/si";
 import { BsPhoneVibrateFill } from "react-icons/bs";
 import { AiOutlineGlobal } from "react-icons/ai";
+import { CgMenuGridO } from "react-icons/cg";
+import Logo from "../../assets/Logo.png";
 
 const Navbar = () => {
     return (
@@ -24,9 +26,33 @@ const Navbar = () => {
             </div>
 
             <div className="navBartTwo">
+
                 <div className="logoDiv">
-                    <img src="" alt="" />
+                    <img src={Logo} />
                 </div>
+
+                <div className="navBarMenu">
+                    <ul className="menu flex">
+                        <li className="listItem">Home</li>
+                        <li className="listItem">About</li>
+                        <li className="listItem">Offers</li>
+                        <li className="listItem">Seats</li>
+                        <li className="listItem">Destinations</li>
+                    </ul>
+
+                    <button className="btn flex btnOne">
+                        Contact
+                    </button>
+                </div>
+
+                {/* <button className="btn flex btnTwo">
+                    Contact
+                </button> */}
+
+                <div className="toggleIcon">
+                <CgMenuGridO />
+                </div>
+
             </div>
         </div>
 
