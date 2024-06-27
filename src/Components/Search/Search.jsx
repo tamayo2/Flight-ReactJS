8f19947fc522201d8f6d8458fe05e362f5044f08
tamayo2 <juan.tamayo3@utp.edu.co>
2024-06-27 11:10:33 -0500

    asignaciones

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -2,7 +2,28 @@ import React from "react";
 
 const Search = () => {
     return (
-        <div></div>
+        <div className="search container section">
+            <div className="sectionContainer grid">
+
+                <div className="btns flex">
+
+                    <div className="singleBtn">
+                        <span>Economy</span>
+                    </div>
+
+                    <div className="singleBtn">
+                        <span>Economy</span>
+                    </div>
+
+                    <div className="singleBtn">
+                        <span>Economy</span>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
     )
 }
 
