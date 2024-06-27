13cf3876c375a8b63ac9ec2631ea41d89d404c93
tamayo2 <juan.tamayo3@utp.edu.co>
2024-06-27 16:18:41 -0500

    Section Search

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { RiAccountPinCircleLine } from "react-icons/ri";
+import { RxCalendar } from "react-icons/rx";
 
 const Search = () => {
     return (
@@ -12,19 +15,65 @@ const Search = () => {
                     </div>
 
                     <div className="singleBtn">
-                        <span>Economy</span>
+                        <span>Business Class</span>
                     </div>
 
                     <div className="singleBtn">
-                        <span>Economy</span>
+                        <span>First Class</span>
                     </div>
 
                 </div>
-            </div>
 
+                <div className="searchInputs flex">
+
+                    <div className="singleInput flex">
+                        <div className="iconDiv">
+                            <HiOutlineLocationMarker className="icon" />  
+                        </div>
+                        <div className="texts">
+                            <h4>Location</h4>
+                            <input type="text" placeholder="Donde sera tu destino" />
+                        </div>
+                    </div>
+
+                    <div className="singleInput flex">
+                        <div className="iconDiv">
+                            <RiAccountPinCircleLine className="icon" />  
+                        </div>
+                        <div className="texts">
+                            <h4>Travelers</h4>
+                            <input type="text" placeholder="Añade una pregunta" />
+                        </div>
+                    </div>
+
+                    <div className="singleInput flex">
+                        <div className="iconDiv">
+                            <RxCalendar className="icon" />  
+                        </div>
+                        <div className="texts">
+                            <h4>Check in</h4>
+                            <input type="text" placeholder="Añade fechas" />
+                        </div>
+                    </div>
+
+                    <div className="singleInput flex">
+                        <div className="iconDiv">
+                            <RxCalendar className="icon" />  
+                        </div>
+                        <div className="texts">
+                            <h4>Check out</h4>
+                            <input type="text" placeholder="Añade fechas" />
+                        </div>
+                    </div>
+
+                    <button className="btn btnBlock flex">Search Flight</button>
+                </div>
+            </div>
 
         </div>
     )
 }
 
 export default Search;
+
+
