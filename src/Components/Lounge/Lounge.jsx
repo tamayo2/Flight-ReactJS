1ba4ae866881ba53f0350a641752d551ad98d45f
tamayo2 <juan.tamayo3@utp.edu.co>
2024-06-29 13:51:07 -0500

    sabado29

--- src/Components/Lounge/Lounge.jsx
+++ src/Components/Lounge/Lounge.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import imageGrid from "../../assets/image-Grid2.jpg";
 
 const Lounge = () => {
     return (
-        <div></div>
+        <div className="lounge container section">
+            <div className="sectionContainer grid">
+
+               <div className="imgDiv">
+                <img src={imageGrid}/>
+                </div> 
+
+                <div className="textDiv">
+                    <h2>Unaccompanied Minor Lounge</h2>
+                </div>
+
+                <div className="grids grid">
+                    <div className="singleGrid">
+                        <span className="gridTitle">
+                            Help through the airport
+                        </span>
+                        <p>
+                        you can also call airlines to ask about their unaccompanied minor programs.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
