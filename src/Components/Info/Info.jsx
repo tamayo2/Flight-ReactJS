c854a97161a205c12c630538a37f13f6f8d5ff56
tamayo2 <juan.tamayo3@utp.edu.co>
2024-06-29 12:55:06 -0500

    info.jsx

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { RxCalendar } from "react-icons/rx";
 
 const Info = () => {
     return (
-        <div></div>
+        <div className="info section">
+            <div className="infoContainer container">
+
+                <div className="titleDiv flex">
+                    <h2>Travel to Make memories all around the world</h2>
+                    <button className="btn">
+                        View All
+                    </button>
+                </div>
+
+                <div className="cardsDiv grid">
+                    <div className="singleCard grid">
+                        <div className="iconDiv flex">
+                        <RxCalendar />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
