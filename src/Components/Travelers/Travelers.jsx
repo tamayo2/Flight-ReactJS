7fb708ec9c8b39b76f846f75a4d3cc80fc60921e
tamayo2 <juan.tamayo3@utp.edu.co>
2024-06-30 23:01:42 -0500

    v8

--- src/Components/Travelers/Travelers.jsx
+++ src/Components/Travelers/Travelers.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import newyork from "../../assets/newyork.jpg";
 
 const Travelers = () => {
     return (
-        <div></div>
+        <div className="travelers container section">
+            <div className="sectionContainer">
+                <h2>
+                    Top travelers of this month!
+                </h2>
+
+                <div className="travelersContainer grid">
+                    <div className="singleTraveler">
+                        <img src={newyork} className="destinationImage"/>
+                        <div className="travelerDetails">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
