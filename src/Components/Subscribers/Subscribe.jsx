44db15cddea7d27f962f6c55b55dd145e3479317
tamayo2 <juan.tamayo3@utp.edu.co>
2024-07-01 00:59:48 -0500

    subscribe

--- src/Components/Subscribers/Subscribe.jsx
+++ src/Components/Subscribers/Subscribe.jsx
@@ -2,7 +2,15 @@ import React from "react";
 
 const Subscribe = () => {
     return (
-        <div></div>
+        <div className="subscribe section">
+            <div className="sectionContainer container">
+                <h2>Subscribe Newsletters & get latest news</h2>
+                <div className="inputDiv flex">
+                    <input type="text" placeholder="Enter your email address" />
+                    <button className="btn">Subscribe</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
