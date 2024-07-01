634164743f64a23511d3860faf24b313d35b2997
tamayo2 <juan.tamayo3@utp.edu.co>
2024-07-01 00:28:25 -0500

    const travelers

--- src/Components/Travelers/Travelers.jsx
+++ src/Components/Travelers/Travelers.jsx
@@ -2,6 +2,23 @@ import React from "react";
 import newyork from "../../assets/newyork.jpg";
 import traveler1 from "../../assets/traveler1.jpg";
 
+const travelers = [
+    {
+        id: 1,
+        destinationImage: newyork,
+        travelerImage: traveler1,
+        travelerName: "John Doe",
+        socialLink: "@Juan_Tamayo21"
+    },
+
+    {
+        id: 2,
+        destinationImage: newyork,
+        travelerImage: traveler1,
+        travelerName: "John Doe",
+        socialLink: "@Juan_Tamayo21"
+    }
+]
 const Travelers = () => {
     return (
         <div className="travelers container section">
@@ -12,19 +29,25 @@ const Travelers = () => {
 
                 <div className="travelersContainer grid">
 
-                    <div className="singleTraveler">
-                        <img src={newyork} className="destinationImage"/>
+                    {
+                        travelers.map(({id, destinationImage, travelerImage, travelerName, socialLink})=>{
+                            return(
+                                <div key={id} className="singleTraveler">
+                                    <img src={destinationImage} className="destinationImage"/>
 
-                        <div className="travelerDetails">
-                            <div className="travelerPicture">
-                                <img src={traveler1} className="travelerImage"/>
-                            </div>
-                            <div className="travelerName">
-                                <span>John Doe</span>
-                                <p>@Juan_Tamayo21</p>
-                            </div>
-                        </div>
-                    </div>
+                                    <div className="travelerDetails">
+                                        <div className="travelerPicture">
+                                            <img src={travelerImage} className="travelerImage"/>
+                                        </div>
+                                        <div className="travelerName">
+                                            <span>{travelerName}</span>
+                                            <p>{socialLink}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
 
