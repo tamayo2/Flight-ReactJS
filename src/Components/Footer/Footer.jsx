fe2c585e96f50dd4c25669a63fe89ba3b6be0982
tamayo2 <juan.tamayo3@utp.edu.co>
2024-07-01 22:58:05 -0500

    Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,102 @@
 import React from "react";
+import Logo from "../../assets/logo.png";
+import { TiSocialFacebook } from "react-icons/ti";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div></div>
+        <div className="footer">
+            <div className="sectionContainer container grid">
+                <div className="gridOne">
+                    <div className="logoDiv">
+                        <img src={Logo} className="Logo" />
+                    </div>
+                    <p>Your mind should be stronger than your feelings, fly</p>
+                    <div className="socialIcon flex">
+                    <TiSocialFacebook className="icon" />
+                    <FaGithub className="icon" />
+                    <FaLinkedin className="icon" />
+                    <FaWhatsapp className="icon" />
+                    </div>
+                </div>
+
+                <div className="footerLinks">
+                    <span className="linkTitle">Information</span>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">Explore</a>
+                    </li>
+                    <li>
+                        <a href="">Flight Status</a>
+                    </li>
+                    <li>
+                        <a href="">Travel</a>
+                    </li>
+                    <li>
+                        <a href="">Check-In</a>
+                    </li>
+                    <li>
+                        <a href="">Manage tour booking</a>
+                    </li>
+
+
+                </div>
+
+                <div className="footerLinks">
+                    <span className="linkTitle">Quick Guide</span>
+                    <li>
+                        <a href="">FAQ</a>
+                    </li>
+                    <li>
+                        <a href="">Explore</a>
+                    </li>
+                    <li>
+                        <a href="">Flight Status</a>
+                    </li>
+                    <li>
+                        <a href="">Travel</a>
+                    </li>
+                    <li>
+                        <a href="">Check-In</a>
+                    </li>
+                    <li>
+                        <a href="">Manage tour booking</a>
+                    </li>
+
+
+                </div>
+
+                <div className="footerLinks">
+                    <span className="linkTitle">Information</span>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">Explore</a>
+                    </li>
+                    <li>
+                        <a href="">Flight Status</a>
+                    </li>
+                    <li>
+                        <a href="">Travel</a>
+                    </li>
+                    <li>
+                        <a href="">Check-In</a>
+                    </li>
+                    <li>
+                        <a href="">Manage tour booking</a>
+                    </li>
+
+
+                </div>
+
+            </div>
+
+        </div>
     )
 }
 
